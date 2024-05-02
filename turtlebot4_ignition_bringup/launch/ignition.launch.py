bc2f1e03cd79d1b0c9095e17bb18545b6eca55b7
Paolo Leopardi <paolo.leopardi@uni-konstanz.de>
2024-05-02 17:30:10 +0200

    minor

--- turtlebot4_ignition_bringup/launch/ignition.launch.py
+++ turtlebot4_ignition_bringup/launch/ignition.launch.py
@@ -83,13 +83,13 @@ def generate_launch_description():
         launch_arguments=[
             ('ign_args', [LaunchConfiguration('world'),
                           '.sdf',
-                          ' -v 4 -r',
+                          ' -v 4 -r', # -r automatic start simulation
                           ' --gui-config ',
                           PathJoinSubstitution(
                             [pkg_turtlebot4_ignition_bringup,
                              'gui',
                              LaunchConfiguration('model'),
-                             'gui.config'])])
+                             'gui.config'])]) 
         ]
     )
 
